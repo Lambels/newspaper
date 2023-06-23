51ad55d54377cb6c50aaa6bf57df547c0eed074e
Lambels <patrick.arvatu@yahoo.com>
2023-06-23 14:01:12 +0300

    added strong defaults + help

--- cmd/np/main.go
+++ cmd/np/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"os"
+    "path/filepath"
 
 	"github.com/Lambels/newspaper"
 	"github.com/Lambels/newspaper/commands"
@@ -13,49 +13,53 @@ import (
 )
 
 func main() {
-    flagReg, next, err := parseConfig(os.Args[1:])
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	fs := flag.NewFlagSet("np", flag.ExitOnError)
+	flagReg, next, err := parseConfig(fs, os.Args[1:])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    if next == nil {
-        return
-    }
+	if next == nil {
+		return
+	}
 
-    root := commands.NewRoot()
-    todayCmd := commands.NewToday(flagReg)
+	root := commands.NewRoot(fs)
+	todayCmd := commands.NewToday(flagReg)
+    findCmd := commands.NewFind(flagReg)
 
-    root.Subcommands = []*ffcli.Command{
-        todayCmd,
-    }
+	root.Subcommands = []*ffcli.Command{
+		todayCmd,
+        findCmd,
+	}
 
-    root.ParseAndRun(context.Background(), next)
+	if err := root.ParseAndRun(context.Background(), next); err != nil {
+		fmt.Println(err)
+	}
 }
 
-func parseConfig(args []string) (*commands.FlagRegister, []string, error) {
+func parseConfig(fs *flag.FlagSet, args []string) (*commands.FlagRegister, []string, error) {
 	flagReg := new(commands.FlagRegister)
 
-	fs := flag.NewFlagSet("np", flag.ExitOnError)
-	ctx := flagReg.RegisterGlobal(fs)
-	cfgPath := fs.String("config", "~/.config/newspaper.json", "set the path to the config file")
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, nil, err
+	}
+    path := filepath.Join(homeDir, newspaper.DefaultConfigPath)
+	cfgPath := fs.String("config", path, "set the path to the config file")
 
 	if err := fs.Parse(args); err != nil {
 		return nil, nil, err
 	}
 
 	cfg, err := newspaper.LoadConfig(*cfgPath)
-	var pathErr os.PathError
-	switch {
-	case errors.As(err, &pathErr):
-		commands.LogVerbosef(*ctx.Verbose, "could not open config file at: %s , using default config", pathErr.Path)
-	case err != nil:
+	if err != nil {
 		return nil, nil, err
 	}
-    
-    flagReg.Config = cfg
-    if fs.NFlag() == 0 && len(fs.Args()) == 0 {
-        return flagReg, nil, nil    
-    }
-    return flagReg, fs.Args(), nil
+
+	flagReg.Config = cfg
+	if fs.NFlag() != 0 && len(fs.Args()) == 0 {
+		return flagReg, nil, nil
+	}
+	return flagReg, fs.Args(), nil
 }
