c5fece10149d6e26395a417bd15b763308a43c78
Lambels <patrick.arvatu@yahoo.com>
2023-07-02 19:19:33 +0300

    error reporter

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,3 +1,11 @@
 package interpreter
 
+type ErrorReporter struct {}
+
+func (r *ErrorReporter) AddErr(err error) {}
+
+func (r *ErrorReporter) Flush() error {}
+
+func (r *ErrorReporter) HadError() bool {}
+
 type Context struct {}
