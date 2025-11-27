aa6e5ef9deeed670407b4dc15e9570b48f0c97b7
Patrick-Adrian Arvatu <patrick.arvatu@yahoo.com>
2025-11-27 22:39:52 +0100

    implemented pushn

---
+++ internal/pusher/pushn.go
@@ -0,0 +1,34 @@
+package pusher
+
+import "encoding/binary"
+
+// could be a chain of push n times but due to efficiency we will provide native support.
+type PushN struct {
+	n int
+}
+
+func NewPushN(n int) PushN {
+	return PushN{n}
+}
+
+func (p *PushN) Advance() error {
+	if p.n < 1 {
+		return ErrFinished
+	}
+
+	p.n--
+	return ErrPushed
+}
+
+func (p *PushN) AdvanceN(_ int) (int, error) {
+	return 1, p.Advance()
+}
+
+func (p PushN) AppendBinary(buf []byte) ([]byte, error) {
+	if p.n < 1 {
+		return buf, ErrFinished
+	}
+
+	buf = append(buf, PushNCode)
+	return binary.AppendUvarint(buf, uint64(p.n)), nil
+}
