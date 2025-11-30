506dbb80180be7e8c2d6d316b79bce1eab5dd1b3
Patrick-Adrian Arvatu <patrick.arvatu@yahoo.com>
2025-11-30 13:44:38 +0100

    added to repeat functionality

--- internal/pusher/repeat.go
+++ internal/pusher/repeat.go
@@ -3,6 +3,7 @@ package pusher
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 )
 
 type Repeat struct {
@@ -11,8 +12,8 @@ type Repeat struct {
 	n int // the number of repetitions left.
 }
 
-func NewRepeat(n int, f factory) Repeat {
-	return Repeat{f(), f, n-1}
+func NewRepeat(n int, f factory) *Repeat {
+	return &Repeat{f(), f, n-1}
 }
 
 func (p *Repeat) refresh() error {
@@ -62,9 +63,14 @@ func (p Repeat) AppendBinary(buf []byte) ([]byte, error) {
 	buf = append(buf, RepeatCode) // start of repeat sequence
 	buf = binary.AppendUvarint(buf, uint64(p.n))
 
+	// what happens if we create a pusher which is finished before any advance call? We return an error because
+	// because there is no reason for an AppendBinary to fail before any advance calls.
+	// if the err is ErrFinished it is un udeterminate state because the next call to advance will also return
+	// err finished indicating a push but this is not the case since the error was there before the call to push
+	// this cannot be comunicated!
 	buf, err = p.f().AppendBinary(buf)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Unexpected error when appending binary: %w", err)
 	}
 
 	buf = append(buf, EndSeqCode) // end of repeat sequence
@@ -77,5 +83,46 @@ type Forever struct {
 }
 
 
-func NewForever(f factory) Forever {
+func NewForever(f factory) *Forever {
+	return &Forever{f(), f}
+}
+
+func (p *Forever) Advance() error {
+	err := p.p.Advance()
+		
+	if errors.Is(err, ErrFinished) {
+		p.p = p.f()
+		return ErrPushed
+	}
+
+	return err
+}
+
+func (p *Forever) AdvanceN(n int) (int, error) {
+	n, err := p.p.AdvanceN(n)
+
+	if errors.Is(err, ErrFinished) {
+		p.p = p.f()
+		return n, ErrPushed
+	}
+
+	return n, err
+}
+
+func (p Forever) AppendBinary(buf []byte) ([]byte, error) {
+	var err error
+	buf, err = p.p.AppendBinary(buf)
+
+	if err != nil && !errors.Is(err, ErrFinished) {
+		return nil, err
+	}
+
+	buf = append(buf, ForeverCode)
+	buf, err = p.f().AppendBinary(buf)
+	if err != nil {
+		return nil, fmt.Errorf("Unexpected error when appending binary: %w", err) // see Repeat comment.
+	}
+
+	buf = append(buf, EndSeqCode)
+	return buf, nil
 }
